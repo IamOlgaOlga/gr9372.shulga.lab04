2e797a6794191e20adf3842aaa5b3f5360f03511
Your Name <shul_ov@yahoo.com>
2012-06-14 10:29:20 +0300

    1

--- src/gr9372/shulga/minesweeper/view/MinesweeperView.java
+++ src/gr9372/shulga/minesweeper/view/MinesweeperView.java
@@ -2,7 +2,6 @@ package gr9372.shulga.minesweeper.view;
 
 public interface MinesweeperView {
 	
-	//public void setVal(int val);
 	public Object getListElement();
 	public void setTime(int time);
 	public void stopGame(Boolean situation);
